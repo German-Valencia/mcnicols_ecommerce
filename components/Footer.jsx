de83ae18bb3b074918feb9c9313e54eaa817fd16
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2022-07-04 12:08:45 -0500

    sprite

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 McNicols Guitars All Rights Reserved ™ ® & Copyright® </p>
+      <p className="icons">
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
