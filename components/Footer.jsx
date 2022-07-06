96b63b421dbdf2a2e8b97af6f4023726e7df4e52
Germán Alberto Valencia Torres <lenovopromotor@gmail.com>
2022-07-05 19:40:02 -0500

    strip end

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
-
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 McNicols Guitars All Rights Reserved ™ ® & Copyright® </p>
+      <p>
+        2022{" "}
+        <Image
+          src="/../public/logo.png"
+          alt="McNicols"
+          width="65px"
+          height="30px"
+        />
+        Guitars All Rights Reserved ™ ® & Copyright ©
+      </p>
       <p className="icons">
-        <AiFillInstagram/>
-        <AiOutlineTwitter/>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
       </p>
     </div>
   );
